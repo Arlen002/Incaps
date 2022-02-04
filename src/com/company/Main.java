d314b7aefd644947d8e1d18767485edd2479bf6d
Arlen002 <kurbanbaevarlen001@gmail.com>
2022-02-04 12:20:49 +0300

    tapshurma1 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,27 +33,4 @@ public class Main {
 
     }
     }
-//         Laptop laptop = new Laptop();
-//         laptop.setName("Lenovo ");
-//         laptop.setJyly(2019);
-//         laptop.setPamyat(256);
-//         laptop.setColour(" Serebristyi ");
-//         laptop.setProsessor(8);
-//         laptop.sublaptops();
-//
-//        Laptop laptop2 = new Laptop();
-//        laptop2.setName("Аpple ");
-//        laptop2.setJyly(2020);
-//        laptop2.setPamyat(512);
-//        laptop2.setColour(" kok ");
-//        laptop2.setProsessor(16);
-//        Laptop [] laptops = {laptop, laptop2};
-//
-//        Student st = new Student();
-//        st.setName(" Arlen ");
-//        st.setAge(-20);
-//        st.setGroup(" Java 5 ");
-//        st.setLaptop(laptops);
-//        st.massiv();
-//    }
-//}
+
